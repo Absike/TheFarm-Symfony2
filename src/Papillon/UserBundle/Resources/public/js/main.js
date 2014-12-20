cc1cf142047b46f9af4a2b27606650d3daf3b7ac
hassan absike <absike@gmail.com>
2014-12-20 16:03:32 +0000

    using moment js in format date

--- src/Papillon/UserBundle/Resources/public/js/main.js
+++ src/Papillon/UserBundle/Resources/public/js/main.js
@@ -54,3 +54,48 @@ var buildDataTable = function (container,aData) {
 
     return oTable;
 };
+
+/**
+ * Convert UTC time to normal time format using Moment js
+ * @param s
+ * @returns {*}
+ */
+function dateFormatter(s) {
+    return moment.parseZone(s).format('D MMM YYYY');
+}
+
+
+/** Gestion cookies **/
+
+//Set Cookies
+function setCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+//Get Cookies
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+//Check cookies
+function checkCookie(name) {
+
+    if (document.cookie.indexOf(name) > 0 ) {
+        return true
+    }
+
+    return false
+}
